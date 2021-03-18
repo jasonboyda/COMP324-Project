01bb59290c1e72e042b903af90f36350484a9081
Amela Kalezic <amelakalezic@gmail.com>
2021-03-18 14:48:16 -0500

    updated signup.js to save input from user

---
+++ common/js/signup.js
@@ -0,0 +1,33 @@
+function getInfo() {
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var expLvl = null;
+    //Sorry this part is a little bit cluttered, will streamline later
+    if (document.getElementById('novice').checked) {
+        expLvl = document.getElementById('novice').value;
+      }
+    if (document.getElementById('intermediate').checked) {
+        expLvl = document.getElementById('intermediate').value;
+    }
+    if (document.getElementById('advanced').checked) {
+        expLvl = document.getElementById('advanced').value;
+    }
+    //gets the checked areas of interest
+    var checkboxes = document.getElementsByName('area');
+    var vals = "";
+    for (var i=0, n=checkboxes.length;i<n;i++) {
+        if (checkboxes[i].checked) {
+            vals += ", "+checkboxes[i].value;
+        }
+    }
+    //gets username
+    var username = document.getElementById("username").value;
+    //need to figure out how to save the password while protecting user privacy
+    //gets email
+    var email = document.getElementById("email").value;
+}
+//currently not a functional method
+function userProfile() {
+    var body = document.createElement('p');
+    body.innerHTML = [name, age, expLvl, vals, username, email];
+}
