eb442492090d118a87494733ed13c86e10befc6c
Amela Kalezic <amelakalezic@gmail.com>
2021-04-28 22:10:40 -0500

    edited sign in page, not yet fully functional for firebase

--- common/js/scripts.js
+++ common/js/scripts.js
@@ -33,15 +33,15 @@ const login = document.getElementById("login");
 const password = document.getElementById("password");
 const commit = document.getElementById("commit");
     
-commit.addEventListener("commit", e => {
+commit.addEventListener("click", e => {
     // Get email and pass
     const email = login.value;
     const pass = password.value;
     const auth = firebase.auth();
     console.log(email, pass)
     // Sign in
-    //const promise = auth.signInWithEmailAndPassword(email, pass);
-    //promise.catch(e => console.log(e.message));
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
     })
 
 
